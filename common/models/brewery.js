ab60dac07f4ca54bef60f81c1ce007184aadc760
alejandrocastrolopez <acalo91@gmail.com>
2016-02-07 20:06:14 -0800

    Added allBrews function

--- common/models/brewery.js
+++ common/models/brewery.js
@@ -1,3 +1,25 @@
 module.exports = function(Brewery) {
 
+	Brewery.allBrews = function(cb)
+	{	
+		var BreweryCollection = Brewery.getDataSource().connector.collection(Brewery.modelName);
+
+		BreweryCollection.aggregate(
+			{"$unwind":"$brews"},
+			{"$project":{"brews":1}},
+			function(err, data)
+			{
+				cb(err, data);
+			}
+		);
+	};
+
+	Brewery.remoteMethod(
+		'allBrews',
+		{
+			accepts:[],
+			returns:{arg:'data'},
+			http:{verb:"GET", status:200, errorStatus:400}
+		}
+	);
 };
