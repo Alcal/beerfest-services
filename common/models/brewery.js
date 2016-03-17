e914cef8e5c15ae6409f2e3f4a8da44d9058dab8
Alejandro Castro <acastro@24hourfit.com>
2016-03-17 15:09:08 -0700

    Revert "Added sorting to Breweries"
    This reverts commit 325a03cb322b69460c7793875d758f96c520f7a1.

--- common/models/brewery.js
+++ common/models/brewery.js
@@ -22,26 +22,4 @@ module.exports = function(Brewery) {
 			http:{verb:"GET", status:200, errorStatus:400}
 		}
 	);
-
-	
-	Brewery.observe('access', function (context, next)
-	{
-		var sortByName = function(a, b)
-		{ 
-			if(a && b)
-		    {
-		      if(a.name == b.name)
-		        return 0;
-		      else if(a.name < b.name)
-		        return -1;
-		      else if (a.name > b.name)
-		        return 1;
-		    }
-		    else
-		      return 0; }
-
-		context.data.sort(sortByName);
-
-		next();
-	});
 };
